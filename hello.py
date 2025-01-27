33ab75701cf687d2a8e26596d46d3ebd02c3cdb3
Никита Войтов <voitov@MacBook-Air-Nikita.local>
2025-01-27 20:04:34 +0300

    1

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from avt-test!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ package_name/capitalize.py
@@ -0,0 +1,7 @@
+def capitalize(text):
+    if text == "":
+        return ""
+    upp = text[0].upper()
+    return f"{upp}{text[1:]}"
+
+print(capitalize("hello"))
